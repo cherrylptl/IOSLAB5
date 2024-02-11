fdd511e241d7c9c3932e9fc1fe42d6616090f762
Cherryl Patel <cherrylptl2260@gmail.com>
2024-02-11 15:07:56 -0500

    IOS LAB-5 Implemented

---
+++ IOSLab5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSLab5
+//
+//  Created by thor on 2024-02-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
